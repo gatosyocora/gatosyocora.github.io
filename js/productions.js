e8abb1bc95f9fc1b7bfdf48fcc195d481333e687
gatosyocora <kut190316q@gmail.com>
2018-03-13 00:03:51 +0900

    change to use json data

--- js/productions.js
+++ js/productions.js
@@ -1,52 +1,53 @@
 <!--
 
 // 自動的に制作物一覧を作成
-window.onload = function() {
-  var name = ["ホームページ", "だんじょん", "バーチャルアシスタント"];
-  var image = ["", "dungeon.png", ""];
-  var abstract = ["このWebページです。<br>GitHubを使ってWebページを公開しています。",
-                  "ステージを回転させたり, キャラクターを動かしたりしながらゴールを目指す2Dアクションゲームです。",
-                  "デスクトップ上でキャラクターと話したり、作業を手伝ってもらったりできるものです。"];
+$(function() {
 
   var createdDiv = document.getElementById("created"); // 制作物一覧を入れる場所を取得
 
-  // 制作物を要素として追加していく
-  for (var i = 0; i < name.length; i++) {
+  $.getJSON("./res/productions.json", function(data) {
 
-    var productionDiv = document.createElement("div");
-    productionDiv.classList.add("production");
-    productionDiv.id = "production"+i;
+    // 制作物を要素として追加していく
+    for (var i = 0; i < data.length; i++) {
 
-    // 名前を管理するdivを作成し追加
-    if (name[i] != "") {
-      var nameDiv = document.createElement("div");
-      nameDiv.id = "name";
-      var h3 = document.createElement("h3");
-      h3.innerHTML = name[i];
-      nameDiv.appendChild(h3);
-      productionDiv.appendChild(nameDiv);
+      var productionDiv = document.createElement("div");
+      productionDiv.classList.add("production");
+      productionDiv.id = "production"+i;
+
+      // 名前を管理するdivを作成し追加
+      if (data[i].name != "") {
+        var nameDiv = document.createElement("div");
+        nameDiv.id = "name";
+        var h3 = document.createElement("h3");
+        h3.innerHTML = data[i].name;
+        nameDiv.appendChild(h3);
+        productionDiv.appendChild(nameDiv);
+      }
+
+      // 画像を管理するdivを作成し追加
+      if (data[i].image != "") {
+        var imageDiv = document.createElement("div");
+        imageDiv.id = "image";
+        var img = document.createElement("img");
+        img.src = "./image/"+data[i].image;
+        imageDiv.appendChild(img);
+        productionDiv.appendChild(imageDiv);
+      }
+
+      // 概要を管理するdivを作成し追加
+      if (data[i].abstract != "") {
+        var abstractDiv = document.createElement("div");
+        abstractDiv.id = "abstract";
+        var p = document.createElement("p");
+        p.innerHTML = data[i].abstract;
+        abstractDiv.appendChild(p);
+        productionDiv.appendChild(abstractDiv);
+      }
+
+      createdDiv.appendChild(productionDiv);
     }
 
-    // 画像を管理するdivを作成し追加
-    if (image[i] != "") {
-      var imageDiv = document.createElement("div");
-      imageDiv.id = "image";
-      var img = document.createElement("img");
-      img.src = "./image/"+image[i];
-      imageDiv.appendChild(img);
-      productionDiv.appendChild(imageDiv);
-    }
+  });
 
-    // 概要を管理するdivを作成し追加
-    if (abstract[i] != "") {
-      var abstractDiv = document.createElement("div");
-      abstractDiv.id = "abstract";
-      var p = document.createElement("p");
-      p.innerHTML = abstract[i];
-      abstractDiv.appendChild(p);
-      productionDiv.appendChild(abstractDiv);
-    }
-    createdDiv.appendChild(productionDiv);
-  }
-};
+});
 -->
