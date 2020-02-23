3d102712a47541a69478e76b3bda55aa1ddb2c4b
gatosyocora <kut190316q@gmail.com>
2020-02-24 01:36:48 +0900

    vrmファイルの3dモデルを表示できるようにした

--- js/index.js
+++ js/index.js
@@ -1,30 +1,72 @@
-window.onload = CreateWorks();
+var mask = document.getElementById('grayfilter');
 
-function CreateWorks() {
-  var works = document.getElementById('works');
+// renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setPixelRatio( window.devicePixelRatio );
+document.body.appendChild( renderer.domElement );
 
-  $.getJSON("./res/works.json", (data) => {
-    var worksData = data.works;
+// camera
+const camera = new THREE.PerspectiveCamera( 30.0, window.innerWidth / window.innerHeight, 0.1, 20.0 );
+camera.position.set( 0.0, 1.0, 5.0 );
 
-    for (let i = 0; i < worksData.length; i++) {
-      let workdiv = document.createElement('div');
-      workdiv.id = worksData[i].name;
-      workdiv.class = "work fly";
-      workdiv.onclick = "window.open("+worksData[i].url+",'_blank')";
-      works.appendChild(workdiv);
+// camera controls
+const controls = new THREE.OrbitControls( camera, renderer.domElement );
+controls.screenSpacePanning = true;
+controls.target.set( 0.0, 1.0, 0.0 );
+controls.update();
 
-      let worknamediv = document.createElement('div');
-      worknamediv.class = "work-name";
-      worknamediv.textContent = worksData[i].name;
-      workdiv.appendChild(worknamediv);
+// scene
+const scene = new THREE.Scene();
 
-      let workimg = document.createElement('img');
-      let imageurl = worksData[i].imageurl;
-      if (imageurl == "") {
-        imageurl = "./image/work/noimage.png";
-      }
-      workimg.src = imageurl;
-      workdiv.appendChild(workimg);
-    }
-  });
+// light
+const light = new THREE.DirectionalLight( 0xffffff );
+light.position.set( 1.0, 1.0, 1.0 ).normalize();
+scene.add( light );
+
+// gltf and vrm
+const loader = new THREE.GLTFLoader();
+loader.crossOrigin = 'anonymous';
+loader.load(
+
+  // URL of the VRM you want to load
+  './res/ukon_gatosyocora.vrm',
+
+  // called when the resource is loaded
+  ( gltf ) => {
+    // generate VRM instance from gltf
+    THREE.VRM.from( gltf ).then( ( vrm ) => {
+
+      console.log( vrm );
+      scene.add( vrm.scene );
+
+      vrm.humanoid.getBoneNode( THREE.VRMSchema.HumanoidBoneName.Hips ).rotation.y = Math.PI;
+
+    } );
+
+  },
+
+  // called while loading is progressing
+  ( progress ) => console.log( 'Loading model...', 100.0 * ( progress.loaded / progress.total ), '%' ),
+
+  // called when loading has errors
+  ( error ) => console.error( error )
+
+);
+
+// helpers
+const gridHelper = new THREE.GridHelper( 10, 10 );
+scene.add( gridHelper );
+
+const axesHelper = new THREE.AxesHelper( 5 );
+scene.add( axesHelper );
+
+function animate() {
+
+  requestAnimationFrame( animate );
+
+  renderer.render( scene, camera );
+
 }
+
+animate();
